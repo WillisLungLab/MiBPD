c3261095cd897889c901644a1833fa0d90c12527
Isaac Martin <ashleycmartin999@gmail.com>
2024-08-16 15:57:38 -0500

    Add files via upload

--- Fig S7.R
+++ Fig S7.R
@@ -1,132 +1,406 @@
-library(tidyverse)
-library(phyloseq)
-library(vegan)
-library(viridis)
-library(ecole)
-
-############################FIGURE S7A: ITS PCOA BY OXYGEN CONCENTRATION############################
-
-exp2_fung_rel_prev <- transform_sample_counts(exp2_fung_prev, function(x) x / sum(x) )
-
-exp2_fung_otu_rel <- as.data.frame(t(exp2_fung_rel_prev@otu_table))
-exp2_fung_tax_rel <- as.data.frame(exp2_fung_rel_prev@tax_table)
-exp2_fung_meta_rel <- as.data.frame(exp2_fung_rel_prev@sam_data)
-
-exp2_fung_meta_rel2 <- exp2_fung_meta_rel %>% 
-  mutate(Oxygen_Weeks_Binned = case_when(  exp2_fung_meta_rel$Oxygen_Weeks <= 5 ~ "0-5",
-                                           exp2_fung_meta_rel$Oxygen_Weeks <= 10 ~ "6-10",
-                                           exp2_fung_meta_rel$Oxygen_Weeks <= 15 ~ "11-15",
-                                           exp2_fung_meta_rel$Oxygen_Weeks <= 20 ~ "16-20",
-                                           exp2_fung_meta_rel$Oxygen_Weeks <= 26 ~ "21-26"
-  )
-  )
-
-###Ordinate using Bray-Curtis dissimilarity###
-exp2_fung_rel_bray = vegdist(exp2_fung_otu_rel, method='bray')
-exp2_fung_rel_pcoa <- ape::pcoa(exp2_fung_rel_bray)
-exp2_fung_rel_pcoa$values
-
-###Set variable of interest and gradient color palette###
-factor_exp2_ga <- ordered(exp2_fung_meta_rel2$Oxygen_Weeks_Binned, levels=c('0-5', '6-10', '11-15', '16-20','21-26'))
-type_exp2_ga <- as.numeric(factor_exp2_ga)
-pcoa_pal_ga <- c("#fde725","#5ec962","#21918c","#3b528b","#440154")
+library(mia)
+library(miaViz)
+library(DirichletMultinomial)
+library(reshape2)
+library(ggrepel)
 
 
-###Plot PCoA###
-dpi=600
-tiff("Fig E6A Binned.tif", width=5*dpi, height=5*dpi, res=dpi)
-plot(c(-0.4, 0.5), c(-0.5, 0.4), font = 2, font.lab = 2, xlab="PC1", ylab="PC2", type="n")
-points(exp2_fung_rel_pcoa$vectors[,1:2], pch = 21, cex = 1.3, bg = pcoa_pal_ga[type_exp2_ga], lwd = 1)
-dev.off()
+############################FIGURE S7A: ALL ITS DMM LAPLACE PLOT############################
+exp2_fung_tse <- makeTreeSEFromPhyloseq(exp2_fung_prev)
 
-###Create legend###
-tiff("Legend_ITS.tif", width=5*dpi, height=5*dpi, res=dpi)
-legend_image <- as.raster(matrix(rev(pcoa_pal_ga), ncol=1))
-plot(c(0,2),c(0,1),type = 'n', axes = F,xlab = '', ylab = '', main = 'Weeks on Oxygen')
-text(x=1.5, y = seq(0,1,l=5), labels = seq(1))
-rasterImage(legend_image, 0, 0, 1,1)
-dev.off()
-
-###PERMANOVA, omit any NA value###
-permanova_pcoa_df <- data.frame(exp2_fung_meta_rel2)
 set.seed(1312)
-permanova_fung_oxygen_weeks <- vegan::adonis2(exp2_fung_otu_rel ~ Oxygen_Weeks_Binned, data = permanova_pcoa_df, method="bray", na.action = na.omit,permutations = 10000)
-print(permanova_fung_oxygen_weeks)
+tse_dmn <- mia::runDMN(exp2_fung_tse, name = "DMN", k = 1:7)
+getDMN(tse_dmn)
 
-###PERMDISP###
-fung_pcoa_dist <- vegdist(exp2_fung_otu_rel_noNA, method = "bray")
-disp_fung_pcoa <- betadisper(fung_pcoa_dist, permanova_pcoa_df_noNA$Oxygen_Weeks_Binned)
+###Since 2 and 5 clusters had similar values, we ran both###
+###Fitting model for 5 clusters###
+plotDMNFit(tse_dmn, type = "laplace")
+#Save
+best_dmn <- getBestDMNFit(tse_dmn, type = "laplace")
+
+
+############################FIGURE S7B: ALL ITS DMM VARIABLE IMPORTANCE############################
+
+#Get fitted values
+for (k in seq(ncol(fitted(best_dmn)))) {
+  d <- melt(fitted(best_dmn))
+  colnames(d) <- c("OTU", "cluster", "value")
+  print(d)
+}
+
+###Get important variables and arrange them in descending order###
+exp2_fung_tax_rel_genonly <- data.frame(new_col = c(exp2_fung_tax_rel$Genus, exp2_fung_tax_rel$Genus, exp2_fung_tax_rel$Genus, exp2_fung_tax_rel$Genus, exp2_fung_tax_rel$Genus))
+d <- d %>%
+  mutate(Genus = exp2_fung_tax_rel_genonly) %>%
+  arrange(value) %>%
+  mutate(OTU = factor(OTU, levels = unique(OTU))) %>%
+  # Only show the most important drivers
+  filter(abs(value) > quantile(abs(value), 0.80))
+print(d)
+
+###Plot Cluster 1###
+d1 <- subset(d, d$cluster == 1)
+d1 <- d1 %>% top_n(10, value)
+p1 <- ggplot(d1, aes(x = reorder(Genus$new_col, value), y = value)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  labs(title = paste("Top drivers: community type 1")) +
+  theme_bw()
+print(p1)
+#Save
+
+###Plot Cluster 2###
+d2 <- subset(d, d$cluster == 2)
+d2 <- d2 %>% top_n(10, value)
+p2 <- ggplot(d2, aes(x = reorder(Genus$new_col, value), y = value)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  labs(title = paste("Top drivers: community type 2")) +
+  theme_bw()
+print(p2)
+#Save
+
+###Plot Cluster 3###
+d3 <- subset(d, d$cluster == 3)
+d3 <- d3 %>% top_n(10, value)
+p3 <- ggplot(d3, aes(x = reorder(Genus$new_col, value), y = value)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  labs(title = paste("Top drivers: community type 3")) +
+  theme_bw()
+print(p3)
+#Save
+
+###Plot Cluster 4###
+d4 <- subset(d, d$cluster ==4)
+d4 <- d4 %>% top_n(10, value)
+p4 <- ggplot(d4, aes(x = reorder(Genus$new_col, value), y = value)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  labs(title = paste("Top drivers: community type 4")) +
+  theme_bw()
+print(p4)
+#Save
+
+###Plot Cluster 5###
+d5 <- subset(d, d$cluster == 5)
+d5 <- d5 %>% top_n(10, value)
+p5 <- ggplot(d5, aes(x = reorder(Genus$new_col, value), y = value)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  labs(title = paste("Top drivers: community type 5")) +
+  theme_bw()
+print(p5)
+#Save
+
+
+############################FIGURE S7C: ALL ITS DMM CCA############################
+
+#Read in metadata file with clinical data (not provided on GitHub)
+
+md_fung_newfactors_sort$cluster <- vec
+exp2_fung_meta_df <- as.data.frame(md_fung_newfactors_sort[,c(1,3,4,6,7,8,25,28,29,30)])
+exp2_fung_meta_df[sapply(exp2_fung_meta_df, is.character)] <- lapply(exp2_fung_meta_df[sapply(exp2_fung_meta_df, is.character)], as.factor)
+sapply(exp2_fung_meta_df, base::class)
+
+exp2_fung_meta_df$BPD<- factor(exp2_fung_meta_df$BPD, levels=c('No_BPD', 'BPD'))
+exp2_fung_meta_df$Died<- factor(exp2_fung_meta_df$Died, levels=c('Survived', 'Died'))
+exp2_fung_meta_df$cluster<- factor(exp2_fung_meta_df$cluster, levels=c('Cluster 4','Cluster 2','Cluster 3','Cluster 1','Cluster 5'))
+
+cca_result2 <- vegan::cca(exp2_fung_otu_rel,exp2_fung_meta_df)
+
+
+#summary of the data
+summary(cca_result2)
+plot(cca_result2)
+
+#extracting data: clinical metadata
+veg_1 = as.data.frame(cca_result2$CCA$biplot)
+row.names(veg_1) <- c("BPD","Died","AMAB","GA","BW","CRIB.II","CRP","Low_Evenness","Fungi_Prev","Cluster 2","Cluster 3","Cluster 1","Cluster 5")
+veg_1["env"] = row.names(veg_1)
+
+#extracting data: samples
+veg_2 = as.data.frame(cca_result2$CCA$u)
+veg_2["samples"] = row.names(veg_2)
+
+#extracting data: taxa
+veg_3 = as.data.frame(cca_result2$CCA$v)
+veg_3["genus"] = row.names(veg_3)
+
+
+#Plot points
+plot = ggplot() +
+  geom_point(data = veg_3, aes(x = CCA1, y = CCA2), color = "#AFEE91", size = 1.0) +
+  geom_point(data = veg_2, aes(x = CCA1, y = CCA2), color = "#0B8700", size = 2.0) +
+  geom_point(data = veg_1, aes(x = CCA1, y = CCA2), color = "black")
+
+
+plot
+
+#Plot arrows and labels
+plot +
+  theme_bw() +
+  geom_segment(
+    data = veg_1,
+    aes(
+      x = 0,
+      y = 0,
+      xend = CCA1,
+      yend = CCA2
+    ),
+    arrow = arrow(length = unit(0.5, "cm"))
+  ) +
+  geom_text_repel(
+    data = veg_1,
+    aes(x = CCA1, y = CCA2, label = veg_1$env),
+    nudge_y = -0.05,
+    color = "black",
+    size = 3
+  ) +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 18))
+#Save
+
+
+############################FIGURE S7D: PPRD ONLY ITS DMM LAPLACE PLOT############################
+
+exp2_fung_tse_pprd <- makeTreeSEFromPhyloseq(exp2_fung_prev_pprd)
+
 set.seed(1312)
-permdisp_fung_pcoa <- permutest(disp_fung_pcoa, permutations = 10000)
+tse_dmn_pprd <- mia::runDMN(exp2_fung_tse_pprd, name = "DMN", k = 1:7)
+getDMN(tse_dmn_pprd)
 
-print(permanova_fung_oxygen_weeks)
-print(permdisp_fung_pcoa)
+plotDMNFit(tse_dmn_pprd, type = "laplace")
+#Save
+
+best_dmn_pprd <- getBestDMNFit(tse_dmn_pprd, type = "laplace")
 
 
-############################FIGURE S7B: 16S PCOA BY OXYGEN CONCENTRATION############################
+############################FIGURE S7E: PPRD ONLY ITS DMM VARIABLE IMPORTANCE############################
 
-exp2_bact_rel_prev <- transform_sample_counts(exp2_bact_prev, function(x) x / sum(x) )
+exp2_fung_tax_rel_pprd$Genus <- make.unique(exp2_fung_tax_rel_pprd$Genus)
 
-exp2_bact_otu_rel <- as.data.frame(t(exp2_bact_rel_prev@otu_table))
-exp2_bact_tax_rel <- as.data.frame(exp2_bact_rel_prev@tax_table)
-exp2_bact_meta_rel <- as.data.frame(exp2_bact_rel_prev@sam_data)
+#Get fitted values
+for (k in seq(ncol(fitted(best_dmn_pprd)))) {
+  d_pprd <- melt(fitted(best_dmn_pprd))
+  colnames(d_pprd) <- c("OTU", "cluster", "value")
+  print(d_pprd)
+}
 
-exp2_bact_meta_rel2 <- exp2_bact_meta_rel %>% 
-  mutate(Oxygen_Weeks_Binned = case_when(  exp2_bact_meta_rel$Oxygen_Weeks <= 5 ~ "0-5",
-                                           exp2_bact_meta_rel$Oxygen_Weeks <= 10 ~ "6-10",
-                                           exp2_bact_meta_rel$Oxygen_Weeks <= 15 ~ "11-15",
-                                           exp2_bact_meta_rel$Oxygen_Weeks <= 20 ~ "16-20",
-                                           exp2_bact_meta_rel$Oxygen_Weeks <= 26 ~ "21-26"
-  )
-  )
+#Get top fitted values
+exp2_fung_tax_rel_pprd_genonly <- data.frame(new_col = c(exp2_fung_tax_rel_pprd$Genus, exp2_fung_tax_rel_pprd$Genus))
+d_pprd <- d_pprd %>%
+  mutate(Genus = exp2_fung_tax_rel_pprd_genonly) %>%
+  arrange(value) %>%
+  mutate(OTU = factor(OTU, levels = unique(OTU))) %>%
+  # Only show the most important drivers
+  filter(abs(value) > quantile(abs(value), 0.70))
+print(d_pprd)
 
-###Ordinate using Bray-Curtis dissimilarity###
-exp2_bact_rel_bray = vegdist(exp2_bact_otu_rel, method='bray')
-exp2_bact_rel_pcoa <- ape::pcoa(exp2_bact_rel_bray)
-exp2_bact_rel_pcoa$values
+#Plot Cluster 1
+d1_pprd <- subset(d_pprd, d_pprd$cluster == 1)
+d1_pprd <- d1_pprd %>% top_n(10, value)
+p1_pprd <- ggplot(d1_pprd, aes(x = reorder(Genus$new_col, value), y = value)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  labs(title = paste("Top drivers: community type 1")) +
+  theme_bw()
+print(p1_pprd)
+#Save
 
-###Set variable of interest and gradient color palette###
-factor_exp2_ga <- ordered(exp2_bact_meta_rel2$Oxygen_Weeks_Binned, levels=c('0-5', '6-10', '11-15', '16-20','21-26'))
-type_exp2_ga <- as.numeric(factor_exp2_ga)
-pcoa_pal_ga <- c("#fde725","#5ec962","#21918c","#3b528b","#440154")
+#Plot Cluster 2
+d2_pprd <- subset(d_pprd, d_pprd$cluster == 2)
+d2_pprd <- d2_pprd %>% top_n(10, value)
+p2_pprd <- ggplot(d2_pprd, aes(x = reorder(Genus$new_col, value), y = value)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  labs(title = paste("Top drivers: community type 2")) +
+  theme_bw()
+print(p2_pprd)
+#Save
 
-###Plot PCoA, using same legend as ITS so no need to plot that twice###
-dpi=600
-tiff("Fig E6B Binned.tif", width=5*dpi, height=5*dpi, res=dpi)
-plot(c(-0.6, 0.4), c(-0.4, 0.4), font = 2, font.lab = 2, xlab="PC1", ylab="PC2", type="n")
-points(exp2_bact_rel_pcoa$vectors[,1:2], pch = 21, cex = 1.3, bg = pcoa_pal_ga[type_exp2_ga], lwd = 1)
-dev.off()
 
-###PERMANOVA###
-permanova_pcoa_df <- data.frame(exp2_bact_meta_rel2)
+############################FIGURE S7F: PPRD ONLY ITS DMM CCA############################
+
+md_fung_newfactors_sort_pprd <- md_fung_newfactors_sort[md_fung_newfactors_sort$BPD == "No_BPD",]
+md_fung_newfactors_sort_pprd$cluster <- vec_pprd
+exp2_fung_meta_df_pprd <- as.data.frame(md_fung_newfactors_sort_pprd[,c(1,3,4,6,7,8,25,28,29,30)])
+exp2_fung_meta_df_pprd[sapply(exp2_fung_meta_df_pprd, is.character)] <- lapply(exp2_fung_meta_df_pprd[sapply(exp2_fung_meta_df_pprd, is.character)], as.factor)
+sapply(exp2_fung_meta_df, base::class)
+
+exp2_fung_meta_df_pprd$BPD<- factor(exp2_fung_meta_df_pprd$BPD, levels=c('No_BPD', 'BPD'))
+exp2_fung_meta_df_pprd$Died<- factor(exp2_fung_meta_df_pprd$Died, levels=c('Survived', 'Died'))
+exp2_fung_meta_df_pprd$cluster<- factor(exp2_fung_meta_df_pprd$cluster, levels=c('Cluster 1', 'Cluster 2'))
+cca_result_pprd <- cca(exp2_fung_otu_rel_pprd, exp2_fung_meta_df_pprd)
+
+#summary
+summary(cca_result_pprd)
+plot(cca_result_pprd)
+
+#Extract metadata
+veg_1_pprd = as.data.frame(cca_result_pprd$CCA$biplot)
+row.names(veg_1_pprd) <- c("AMAB","GA","BW","CRIB.II","CRP","Low_Evenness","Fungi_Abundance","Cluster_1")
+veg_1_pprd["env"] = row.names(veg_1_pprd)
+
+#Extract sample data
+veg_2_pprd = as.data.frame(cca_result_pprd$CCA$u)
+veg_2_pprd["samples"] = row.names(veg_2_pprd)
+
+#Extract taxa
+veg_3_pprd = as.data.frame(cca_result_pprd$CCA$v)
+veg_3_pprd["genus"] = row.names(veg_3_pprd)
+
+#Plot points
+plot_pprd = ggplot() +
+  geom_point(data = veg_3_pprd, aes(x = CCA1, y = CCA2), color = "#AFEE91", size = 1.0) +
+  geom_point(data = veg_2_pprd, aes(x = CCA1, y = CCA2), color = "#0B8700", size = 2.0) +
+  geom_point(data = veg_1_pprd, aes(x = CCA1, y = CCA2), color = "black")
+
+
+plot_pprd
+
+#Plot arrows and labels
+plot_pprd +
+  theme_bw() +
+  geom_segment(
+    data = veg_1_pprd,
+    aes(
+      x = 0,
+      y = 0,
+      xend = CCA1,
+      yend = CCA2
+    ),
+    arrow = arrow(length = unit(0.5, "cm"))
+  ) +
+  geom_text_repel(
+    data = veg_1_pprd,
+    aes(x = CCA1, y = CCA2, label = veg_1_pprd$env),
+    nudge_y = -0.05,
+    color = "black",
+    size = 3
+  ) +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 18))
+#Save
+
+
+############################FIGURE S7F: BPD ONLY ITS DMM LAPLACE PLOT############################
+
+exp2_fung_tse_bpd <- makeTreeSEFromPhyloseq(exp2_fung_prev_bpd)
+
 set.seed(1312)
-permanova_bact_oxygen_weeks <- vegan::adonis2(exp2_bact_otu_rel ~ Oxygen_Weeks_Binned, data = permanova_pcoa_df, method="bray", na.action = na.omit,permutations = 10000)
-print(permanova_bact_oxygen_weeks)
-oxygen_weeks_p <- permanova_bact_oxygen_weeks$`Pr(>F)`
+tse_dmn_bpd <- mia::runDMN(exp2_fung_tse_bpd, name = "DMN", k = 1:7)
+getDMN(tse_dmn_bpd)
 
-###Pairwise PERMANOVA###
-permanova_pcoa_df_noNA <-permanova_pcoa_df[-c(2,4,62,65,68,93),]
-exp2_bact_otu_rel_noNA <-exp2_bact_otu_rel[-c(2,4,62,65,68,93),]
+plotDMNFit(tse_dmn_bpd, type = "laplace")
+#Save
 
-pairwise_permanova_bact_oxygen_weeks <- permanova_pairwise(exp2_bact_otu_rel_noNA, grp = permanova_pcoa_df_noNA$Oxygen_Weeks_Binned, permutations = 10000, method = "bray", padj = "fdr")
-
-###PERMDISP###
-bact_pcoa_dist <- vegdist(exp2_bact_otu_rel, method = "bray")
-disp_bact_pcoa <- betadisper(bact_pcoa_dist, permanova_pcoa_df$Oxygen_Weeks_Binned)
-set.seed(1312)
-permdisp_bact_pcoa <- permutest(disp_bact_pcoa, permutations = 10000)
-
-print(permanova_bact_oxygen_weeks)
-print(permdisp_bact_pcoa)
+best_dmn_bpd <- getBestDMNFit(tse_dmn_bpd, type = "laplace")
 
 
-###Legend###
+############################FIGURE S7G: BPD ONLY ITS DMM VARIABLE IMPORTANCE############################
 
-dpi=600
-tiff("Legend_16S.tif", width=5*dpi, height=5*dpi, res=dpi)
-plot(NULL ,xaxt='n',yaxt='n',bty='n',ylab='',xlab='', xlim=0:1, ylim=0:1)
-legend("topleft", legend =c('0-5', '6-10', '11-15',
-                            '16-20', '21-26'), pch=16, pt.cex=3, cex=1.5, bty='n',
-       col = c("#fde725","#5ec962","#21918c","#3b528b","#440154"))
-mtext("Weeks on Oxygen", at=0.1, cex=1.5)
-dev.off()
+exp2_fung_tax_rel_bpd$Genus <- make.unique(exp2_fung_tax_rel_bpd$Genus)
+
+#Get fitted values
+for (k in seq(ncol(fitted(best_dmn_bpd)))) {
+  d_bpd <- melt(fitted(best_dmn_bpd))
+  colnames(d_bpd) <- c("OTU", "cluster", "value")
+  print(d_bpd)
+}
+
+#Get top fitted values
+exp2_fung_tax_rel_bpd_genonly <- data.frame(new_col = c(exp2_fung_tax_rel_bpd$Genus, exp2_fung_tax_rel_bpd$Genus))
+d_bpd <- d_bpd %>%
+  mutate(Genus = exp2_fung_tax_rel_bpd_genonly) %>%
+  arrange(value) %>%
+  mutate(OTU = factor(OTU, levels = unique(OTU))) %>%
+  # Only show the most important drivers
+  filter(abs(value) > quantile(abs(value), 0.70))
+print(d_bpd)
+
+#Plot Cluster 1
+d1_bpd <- subset(d_bpd, d_bpd$cluster == 1)
+d1_bpd <- d1_bpd %>% top_n(10, value)
+p1_bpd <- ggplot(d1_bpd, aes(x = reorder(Genus$new_col, value), y = value)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  labs(title = paste("Top drivers: community type 1")) +
+  theme_bw()
+print(p1_bpd)
+#Save
+
+#Plot Cluster 2
+d2_bpd <- subset(d_bpd, d_bpd$cluster == 2)
+d2_bpd <- d2_bpd %>% top_n(10, value)
+p2_bpd <- ggplot(d2_bpd, aes(x = reorder(Genus$new_col, value), y = value)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  labs(title = paste("Top drivers: community type 2")) +
+  theme_bw()
+print(p2_bpd)
+#Save
+
+
+############################FIGURE S7H: BPD ONLY ITS DMM CCA############################
+
+md_fung_newfactors_sort_bpd <- md_fung_newfactors_sort[md_fung_newfactors_sort$BPD == "BPD",]
+md_fung_newfactors_sort_bpd$cluster <- vec_bpd
+exp2_fung_meta_df_bpd <- as.data.frame(md_fung_newfactors_sort_bpd[,c(1,3,4,6,7,8,25,28,29,30)])
+exp2_fung_meta_df_bpd[sapply(exp2_fung_meta_df_bpd, is.character)] <- lapply(exp2_fung_meta_df_bpd[sapply(exp2_fung_meta_df_bpd, is.character)], as.factor)
+sapply(exp2_fung_meta_df, base::class)
+
+exp2_fung_meta_df_bpd$BPD<- factor(exp2_fung_meta_df_bpd$BPD, levels=c('No_BPD', 'BPD'))
+exp2_fung_meta_df_bpd$Died<- factor(exp2_fung_meta_df_bpd$Died, levels=c('Survived', 'Died'))
+exp2_fung_meta_df_bpd$cluster<- factor(exp2_fung_meta_df_bpd$cluster, levels=c('Cluster 2', 'Cluster 1'))
+cca_result_bpd <- cca(exp2_fung_otu_rel_bpd, exp2_fung_meta_df_bpd)
+
+#summary of the data
+summary(cca_result_bpd)
+plot(cca_result_bpd)
+
+#Extract metadata
+veg_1_bpd = as.data.frame(cca_result_bpd$CCA$biplot)
+row.names(veg_1_bpd) <- c("Died","AMAB","GA","BW","CRIB.II","CRP","Low_Evenness","Fungi_Abundance","Cluster_1")
+veg_1_bpd["env"] = row.names(veg_1_bpd)
+
+#Extract sample data
+veg_2_bpd = as.data.frame(cca_result_bpd$CCA$u)
+veg_2_bpd["samples"] = row.names(veg_2_bpd)
+
+#Extract taxa
+veg_3_bpd = as.data.frame(cca_result_bpd$CCA$v)
+veg_3_bpd["genus"] = row.names(veg_3_bpd)
+
+#Plot points
+plot_bpd = ggplot() +
+  geom_point(data = veg_3_bpd, aes(x = CCA1, y = CCA2), color = "#AFEE91", size = 1.0) +
+  geom_point(data = veg_2_bpd, aes(x = CCA1, y = CCA2), color = "#0B8700", size = 2.0) +
+  geom_point(data = veg_1_bpd, aes(x = CCA1, y = CCA2), color = "black")
+
+
+plot_bpd
+
+#Plot arrows and labels
+plot_bpd +
+  theme_bw() +
+  geom_segment(
+    data = veg_1_bpd,
+    aes(
+      x = 0,
+      y = 0,
+      xend = CCA1,
+      yend = CCA2
+    ),
+    arrow = arrow(length = unit(0.5, "cm"))
+  ) +
+  geom_text_repel(
+    data = veg_1_bpd,
+    aes(x = CCA1, y = CCA2, label = veg_1_bpd$env),
+    nudge_y = -0.05,
+    color = "black",
+    size = 3
+  ) +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 18))
+#Save
